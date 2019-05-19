f7bca9ee4f6774c86afcee01101d8707d50a922b
Alan Barnett <alanbarnett328@gmail.com>
2019-05-18 19:12:02 -0700

    feat(launch_function): launch and save return
    the launch_function function will fork the current process, call the function
    pointer with the test case, and then wait for the child to finish to
    access it's value. It does not yet check if the process seg faulted or
    bus errored or anything, and at the moment I'm not actually convinced
    that it properly gets the return status either. But this commit also
    enables the launch_tests function to call multiple tests, and free each
    node and the final queue when it's completed.

--- framework/launch_tests.c
+++ framework/launch_tests.c
@@ -6,15 +6,48 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 14:47:53 by abarnett          #+#    #+#             */
-/*   Updated: 2019/05/18 17:47:43 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/05/18 19:04:30 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
+#include "ft_mem.h"
 #include "ft_printf.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void	launch_function(int (*func)(void))
+{
+	pid_t	new_pid;
+	int		*status;
+
+	status = 0;
+	new_pid = fork();
+	if (new_pid == 0)
+	{
+		exit(func());
+	}
+	else
+	{
+		wait(status);
+		ft_printf("[ %d ]\n", WEXITSTATUS(status));
+	}
+}
 
 int		launch_tests(t_unit_test *tests)
 {
-	ft_printf("launch: %s\n", tests->first->name);
+	int		(*function)(void);
+
+	if (!tests)
+		return (0);
+	while (tests->first)
+	{
+		ft_printf("  > %s : ", tests->first->name);
+		function = pop_function(tests);
+		launch_function(function);
+	}
+
+	ft_memdel((void **)&tests);
 	return (0);
 }
