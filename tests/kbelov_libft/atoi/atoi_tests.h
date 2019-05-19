1548da4566257641b2a88711f1e38a39cf797de6
Konstantin Belov <kbelov@e1z2r4p21.42.us.org>
2019-05-19 00:32:08 -0700

    more tests

--- tests/kbelov_libft/atoi/atoi_tests.h
+++ tests/kbelov_libft/atoi/atoi_tests.h
@@ -6,13 +6,33 @@
 /*   By: kbelov <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 18:33:30 by kbelov            #+#    #+#             */
-/*   Updated: 2019/05/18 18:35:46 by kbelov           ###   ########.fr       */
+/*   Updated: 2019/05/19 00:30:37 by kbelov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ATOI_TESTS_H
 # define ATOI_TESTS_H
 
-int	int_min(void);
+int		int_min(void);
+int		int_negative_one(void);
+int		int_zero(void);
+int		int_positive_one(void);
+int		int_max(void);
+int		less_then_int(void);
+int		more_then_int(void);
+int		int_empty_string(void);
+int		int_space_pos(void);
+int		int_space_neg(void);
+int		int_double_neg(void);
+int		int_double_pos(void);
+int		int_single_pos(void);
+int		int_space_neg(void);
+int		int_space_pos(void);
+int		int_space_after(void);
+int		two_ints(void);
+int		no_ints(void);
+int		no_ints_minus(void);
+int		no_ints_plus(void);
+int		space_only(void);
 
 #endif
