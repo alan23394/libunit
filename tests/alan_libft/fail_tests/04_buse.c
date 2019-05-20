8177b5bdbd5fc13c82a51811703e786e354ce1ae
Alan Barnett <alanbarnett328@gmail.com>
2019-05-19 22:49:18 -0700

    fix(fail tests): cleanup
    Make static function for the testing, and make sure the scope is aligned.

--- tests/alan_libft/fail_tests/04_buse.c
+++ tests/alan_libft/fail_tests/04_buse.c
@@ -6,20 +6,25 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 18:40:28 by abarnett          #+#    #+#             */
-/*   Updated: 2019/05/19 18:43:02 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/05/19 22:36:32 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	buse_test(void)
-{
-	int	*num;
-	int	real_num;
+#include <signal.h>
+#include <unistd.h>
 
-	real_num = 10;
-	num = &real_num;
-	++num;
-	*num = 12;
-	if (*num == 12)
+static int	bus_error_function(void)
+{
+	pid_t	pid;
+
+	pid = getpid();
+	kill(pid, 10);
+	return (1);
+}
+
+int			buse_test(void)
+{
+	if (bus_error_function())
 		return (0);
 	else
 		return (1);
