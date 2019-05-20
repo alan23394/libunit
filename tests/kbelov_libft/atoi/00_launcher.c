d56b6c68ace2b79bfa458f19c81f04bb206595b1
Alan Barnett <alanbarnett328@gmail.com>
2019-05-19 19:38:57 -0700

    feat(atoi_launcher): add all the tests and run them
    Switched testlist to tests, used the init() function, and added all
    the tests and ran them.

--- tests/kbelov_libft/atoi/00_launcher.c
+++ tests/kbelov_libft/atoi/00_launcher.c
@@ -16,10 +16,30 @@
 
 int		atoi_launcher(void)
 {
-	t_unit_test	*testlist;
+	t_unit_test	*tests;
 
-	testlist = 0;
+	tests = init();
 	ft_putstr("ATOI:\n");
-	load_test(&testlist, "Int Min", &int_min);
-	return (launch_tests(&testlist));
+	load_test(tests, "int_min", &int_min);
+	load_test(tests, "int_negative_one", &int_negative_one);
+	load_test(tests, "int_zero", &int_zero);
+	load_test(tests, "int_positive_one", &int_positive_one);
+	load_test(tests, "int_max", &int_max);
+	load_test(tests, "less_then_int", &less_then_int);
+	load_test(tests, "more_then_int", &more_then_int);
+	load_test(tests, "int_empty_string", &int_empty_string);
+	load_test(tests, "int_space", &int_space);
+	load_test(tests, "int_space_after_neg", &int_space_after_neg);
+	load_test(tests, "int_double_neg", &int_double_neg);
+	load_test(tests, "int_double_pos", &int_double_pos);
+	load_test(tests, "int_single_pos", &int_single_pos);
+	load_test(tests, "int_space_neg", &int_space_neg);
+	load_test(tests, "int_space_pos", &int_space_pos);
+	load_test(tests, "int_space_after", &int_space_after);
+	load_test(tests, "two_ints", &two_ints);
+	load_test(tests, "no_ints", &no_ints);
+	load_test(tests, "no_ints_minus", &no_ints_minus);
+	load_test(tests, "no_ints_plus", &no_ints_plus);
+	load_test(tests, "space_only", &space_only);
+	return (launch_tests(tests));
 }
