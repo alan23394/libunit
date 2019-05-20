6fb5f124228d12dfd86673187a37bfb3dc0a1793
Alan Barnett <alanbarnett328@gmail.com>
2019-05-19 22:50:03 -0700

    update(color): change bus error color
    because A E S T H E T I C S

--- includes/colors.h
+++ includes/colors.h
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 17:47:00 by abarnett          #+#    #+#             */
-/*   Updated: 2019/05/19 18:01:00 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/05/19 22:32:35 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # define COLOR_OK "\e[1;32m"
 # define COLOR_KO "\e[1;31m"
 # define COLOR_SEGV "\e[1;33m"
-# define COLOR_BUSE "\e[1;33m"
+# define COLOR_BUSE "\e[1;35m"
 # define COLOR_UNK "\e[1;34m"
 # define COLOR_NORM "\e[m"
 
